73900b2381e8ec9fdeaece08c8d58118a0d23f8f
Alexander F. Rødseth <rodseth@gmail.com>
2021-02-02 18:05:11 +0100

    Format all source code
    + use the name "ion"

--- include/c.h
+++ include/c.h
@@ -1,38 +1,35 @@
 /*
  * Fundamental C definitions.
- *
- * No copyright is claimed.  This code is in the public domain; do with
- * it what you wish.
  */
-#ifndef UTIL_LINUX_C_H
-#define UTIL_LINUX_C_H
+#ifndef ION_C_H
+#define ION_C_H
 
+#include <errno.h>
 #include <limits.h>
+#include <stdarg.h>
 #include <stddef.h>
 #include <stdint.h>
 #include <stdio.h>
-#include <unistd.h>
-#include <stdarg.h>
 #include <stdlib.h>
 #include <string.h>
-#include <errno.h>
+#include <unistd.h>
 
 #include <assert.h>
 
 #ifdef HAVE_ERR_H
-# include <err.h>
+#include <err.h>
 #endif
 
 #ifdef HAVE_SYS_SYSMACROS_H
-# include <sys/sysmacros.h>     /* for major, minor */
+#include <sys/sysmacros.h> /* for major, minor */
 #endif
 
 #ifndef LOGIN_NAME_MAX
-# define LOGIN_NAME_MAX 256
+#define LOGIN_NAME_MAX 256
 #endif
 
 #ifndef NAME_MAX
-# define NAME_MAX PATH_MAX
+#define NAME_MAX PATH_MAX
 #endif
 
 /*
@@ -40,28 +37,29 @@
  * __has_feature(). The __has macros are supported by clang and gcc>=5.
  */
 #ifndef __GNUC_PREREQ
-# if defined __GNUC__ && defined __GNUC_MINOR__
-#  define __GNUC_PREREQ(maj, min) \
+#if defined __GNUC__ && defined __GNUC_MINOR__
+#define __GNUC_PREREQ(maj, min) \
     ((__GNUC__ << 16) + __GNUC_MINOR__ >= ((maj) << 16) + (min))
-# else
-#  define __GNUC_PREREQ(maj, min) 0
-# endif
+#else
+#define __GNUC_PREREQ(maj, min) 0
+#endif
 #endif
 
 #ifdef __GNUC__
 
 /* &a[0] degrades to a pointer: a different type from an array */
-# define __must_be_array(a) \
+#define __must_be_array(a) \
     UL_BUILD_BUG_ON_ZERO(__builtin_types_compatible_p(__typeof__(a), __typeof__(&a[0])))
 
-# define ignore_result(x) __extension__ ({ \
-    __typeof__(x) __dummy __attribute__((__unused__)) = (x); (void) __dummy; \
+#define ignore_result(x) __extension__({                     \
+    __typeof__(x) __dummy __attribute__((__unused__)) = (x); \
+    (void)__dummy;                                           \
 })
 
 #else /* !__GNUC__ */
-# define __must_be_array(a)	0
-# define __attribute__(_arg_)
-# define ignore_result(x) ((void) (x))
+#define __must_be_array(a) 0
+#define __attribute__(_arg_)
+#define ignore_result(x) ((void)(x))
 #endif /* !__GNUC__ */
 
 /*
@@ -69,36 +67,36 @@
  * compilation target. Fallback for old compilers.
  */
 #ifndef __has_attribute
-  #define __has_attribute(x) 0
+#define __has_attribute(x) 0
 #endif
 
 #ifndef __has_feature
-  #define __has_feature(x) 0
+#define __has_feature(x) 0
 #endif
 
 /*
  * Function attributes
  */
 #ifndef __ul_alloc_size
-# if (__has_attribute(alloc_size) && __has_attribute(warn_unused_result)) || __GNUC_PREREQ (4, 3)
-#  define __ul_alloc_size(s) __attribute__((alloc_size(s), warn_unused_result))
-# else
-#  define __ul_alloc_size(s)
-# endif
+#if (__has_attribute(alloc_size) && __has_attribute(warn_unused_result)) || __GNUC_PREREQ(4, 3)
+#define __ul_alloc_size(s) __attribute__((alloc_size(s), warn_unused_result))
+#else
+#define __ul_alloc_size(s)
+#endif
 #endif
 
 #ifndef __ul_calloc_size
-# if (__has_attribute(alloc_size) && __has_attribute(warn_unused_result)) || __GNUC_PREREQ (4, 3)
-#  define __ul_calloc_size(n, s) __attribute__((alloc_size(n, s), warn_unused_result))
-# else
-#  define __ul_calloc_size(n, s)
-# endif
+#if (__has_attribute(alloc_size) && __has_attribute(warn_unused_result)) || __GNUC_PREREQ(4, 3)
+#define __ul_calloc_size(n, s) __attribute__((alloc_size(n, s), warn_unused_result))
+#else
+#define __ul_calloc_size(n, s)
+#endif
 #endif
 
-#if __has_attribute(returns_nonnull) || __GNUC_PREREQ (4, 9)
-# define __ul_returns_nonnull __attribute__((returns_nonnull))
+#if __has_attribute(returns_nonnull) || __GNUC_PREREQ(4, 9)
+#define __ul_returns_nonnull __attribute__((returns_nonnull))
 #else
-# define __ul_returns_nonnull
+#define __ul_returns_nonnull
 #endif
 
 /*
@@ -107,27 +105,27 @@
  * e.g. in a structure initializer (or wherever else comma expressions
  * aren't permitted).
  */
-#define UL_BUILD_BUG_ON_ZERO(e) __extension__ (sizeof(struct { int:-!!(e); }))
-#define BUILD_BUG_ON_NULL(e) ((void *)sizeof(struct { int:-!!(e); }))
+#define UL_BUILD_BUG_ON_ZERO(e) __extension__(sizeof(struct { int : -!!(e); }))
+#define BUILD_BUG_ON_NULL(e) ((void*)sizeof(struct { int : -!!(e); }))
 
 #ifndef ARRAY_SIZE
-# define ARRAY_SIZE(arr) (sizeof(arr) / sizeof((arr)[0]) + __must_be_array(arr))
+#define ARRAY_SIZE(arr) (sizeof(arr) / sizeof((arr)[0]) + __must_be_array(arr))
 #endif
 
 #ifndef PATH_MAX
-# define PATH_MAX 4096
+#define PATH_MAX 4096
 #endif
 
 #ifndef TRUE
-# define TRUE 1
+#define TRUE 1
 #endif
 
 #ifndef FALSE
-# define FALSE 0
+#define FALSE 0
 #endif
 
 #ifndef min
-# define min(x, y) __extension__ ({		\
+#define min(x, y) __extension__({		\
     __typeof__(x) _min1 = (x);		\
     __typeof__(y) _min2 = (y);		\
     (void) (&_min1 == &_min2);		\
@@ -135,7 +133,7 @@
 #endif
 
 #ifndef max
-# define max(x, y) __extension__ ({		\
+#define max(x, y) __extension__({		\
     __typeof__(x) _max1 = (x);		\
     __typeof__(y) _max2 = (y);		\
     (void) (&_max1 == &_max2);		\
@@ -143,7 +141,7 @@
 #endif
 
 #ifndef cmp_numbers
-# define cmp_numbers(x, y) __extension__ ({	\
+#define cmp_numbers(x, y) __extension__({	\
     __typeof__(x) _a = (x);			\
     __typeof__(y) _b = (y);			\
     (void) (&_a == &_b);			\
@@ -151,7 +149,7 @@
 #endif
 
 #ifndef offsetof
-#define offsetof(TYPE, MEMBER) ((size_t) &((TYPE *)0)->MEMBER)
+#define offsetof(TYPE, MEMBER) ((size_t) & ((TYPE*)0)->MEMBER)
 #endif
 
 /*
@@ -161,28 +159,28 @@
  * @member:	the name of the member within the struct.
  */
 #ifndef container_of
-#define container_of(ptr, type, member) __extension__ ({	\
+#define container_of(ptr, type, member) __extension__({	\
     const __typeof__( ((type *)0)->member ) *__mptr = (ptr); \
-    (type *)( (char *)__mptr - offsetof(type,member) );})
+    (type *)( (char *)__mptr - offsetof(type,member) ); })
 #endif
 
 #ifndef HAVE_PROGRAM_INVOCATION_SHORT_NAME
-# ifdef HAVE___PROGNAME
-extern char *__progname;
-#  define program_invocation_short_name __progname
-# else
-#  ifdef HAVE_GETEXECNAME
-#   define program_invocation_short_name \
-        prog_inv_sh_nm_from_file(getexecname(), 0)
-#  else
-#   define program_invocation_short_name \
-        prog_inv_sh_nm_from_file(__FILE__, 1)
-#  endif
+#ifdef HAVE___PROGNAME
+extern char* __progname;
+#define program_invocation_short_name __progname
+#else
+#ifdef HAVE_GETEXECNAME
+#define program_invocation_short_name \
+    prog_inv_sh_nm_from_file(getexecname(), 0)
+#else
+#define program_invocation_short_name \
+    prog_inv_sh_nm_from_file(__FILE__, 1)
+#endif
 static char prog_inv_sh_nm_buf[256];
-static inline char *
-prog_inv_sh_nm_from_file(char *f, char stripext)
+static inline char*
+prog_inv_sh_nm_from_file(char* f, char stripext)
 {
-    char *t;
+    char* t;
 
     if ((t = strrchr(f, '/')) != NULL)
         t++;
@@ -197,13 +195,12 @@ prog_inv_sh_nm_from_file(char *f, char stripext)
 
     return prog_inv_sh_nm_buf;
 }
-# endif
 #endif
-
+#endif
 
 #ifndef HAVE_ERR_H
 static inline void
-errmsg(char doexit, int excode, char adderr, const char *fmt, ...)
+errmsg(char doexit, int excode, char adderr, const char* fmt, ...)
 {
     fprintf(stderr, "%s: ", program_invocation_short_name);
     if (fmt != NULL) {
@@ -222,37 +219,35 @@ errmsg(char doexit, int excode, char adderr, const char *fmt, ...)
 }
 
 #ifndef HAVE_ERR
-# define err(E, FMT...) errmsg(1, E, 1, FMT)
+#define err(E, FMT...) errmsg(1, E, 1, FMT)
 #endif
 
 #ifndef HAVE_ERRX
-# define errx(E, FMT...) errmsg(1, E, 0, FMT)
+#define errx(E, FMT...) errmsg(1, E, 0, FMT)
 #endif
 
 #ifndef HAVE_WARN
-# define warn(FMT...) errmsg(0, 0, 1, FMT)
+#define warn(FMT...) errmsg(0, 0, 1, FMT)
 #endif
 
 #ifndef HAVE_WARNX
-# define warnx(FMT...) errmsg(0, 0, 0, FMT)
+#define warnx(FMT...) errmsg(0, 0, 0, FMT)
 #endif
 #endif /* !HAVE_ERR_H */
 
-
 /* Don't use inline function to avoid '#include "nls.h"' in c.h
  */
-#define errtryhelp(eval) __extension__ ({ \
+#define errtryhelp(eval) __extension__({                          \
     fprintf(stderr, _("Try '%s --help' for more information.\n"), \
-            program_invocation_short_name); \
-    exit(eval); \
+        program_invocation_short_name);                           \
+    exit(eval);                                                   \
 })
 
 /* After failed execvp() */
-#define EX_EXEC_FAILED		126	/* Program located, but not usable. */
-#define EX_EXEC_ENOENT		127	/* Could not find program to exec.  */
-#define errexec(name)	err(errno == ENOENT ? EX_EXEC_ENOENT : EX_EXEC_FAILED, \
-            _("failed to execute %s"), name)
-
+#define EX_EXEC_FAILED 126 /* Program located, but not usable. */
+#define EX_EXEC_ENOENT 127 /* Could not find program to exec.  */
+#define errexec(name) err(errno == ENOENT ? EX_EXEC_ENOENT : EX_EXEC_FAILED, \
+    _("failed to execute %s"), name)
 
 static inline __attribute__((const)) int is_power_of_2(unsigned long num)
 {
@@ -264,9 +259,9 @@ typedef int64_t loff_t;
 #endif
 
 #if !defined(HAVE_DIRFD) && (!defined(HAVE_DECL_DIRFD) || HAVE_DECL_DIRFD == 0) && defined(HAVE_DIR_DD_FD)
-#include <sys/types.h>
 #include <dirent.h>
-static inline int dirfd(DIR *d)
+#include <sys/types.h>
+static inline int dirfd(DIR* d)
 {
     return d->dd_fd;
 }
@@ -278,9 +273,9 @@ static inline int dirfd(DIR *d)
 #include <fcntl.h>
 
 #ifdef O_CLOEXEC
-#define UL_CLOEXECSTR	"e"
+#define UL_CLOEXECSTR "e"
 #else
-#define UL_CLOEXECSTR	""
+#define UL_CLOEXECSTR ""
 #endif
 
 #ifndef O_CLOEXEC
@@ -289,10 +284,9 @@ static inline int dirfd(DIR *d)
 
 #ifdef __FreeBSD_kernel__
 #ifndef F_DUPFD_CLOEXEC
-#define F_DUPFD_CLOEXEC	17	/* Like F_DUPFD, but FD_CLOEXEC is set */
+#define F_DUPFD_CLOEXEC 17 /* Like F_DUPFD, but FD_CLOEXEC is set */
 #endif
 #endif
-
 
 #ifndef AI_ADDRCONFIG
 #define AI_ADDRCONFIG 0x0020
@@ -333,14 +327,14 @@ static inline int xusleep(useconds_t usec)
 {
 #ifdef HAVE_NANOSLEEP
     struct timespec waittime = {
-        .tv_sec   =  usec / 1000000L,
-        .tv_nsec  = (usec % 1000000L) * 1000
+        .tv_sec = usec / 1000000L,
+        .tv_nsec = (usec % 1000000L) * 1000
     };
     return nanosleep(&waittime, NULL);
 #elif defined(HAVE_USLEEP)
     return usleep(usec);
 #else
-# error	"System with usleep() or nanosleep() required!"
+#error "System with usleep() or nanosleep() required!"
 #endif
 }
 
@@ -348,47 +342,46 @@ static inline int xusleep(useconds_t usec)
  * Constant strings for usage() functions. For more info see
  * Documentation/{howto-usage-function.txt,boilerplate.c}
  */
-#define USAGE_HEADER     _("\nUsage:\n")
-#define USAGE_OPTIONS    _("\nOptions:\n")
-#define USAGE_FUNCTIONS  _("\nFunctions:\n")
-#define USAGE_COMMANDS   _("\nCommands:\n")
-#define USAGE_ARGUMENTS   _("\nArguments:\n")
-#define USAGE_COLUMNS    _("\nAvailable output columns:\n")
-#define USAGE_SEPARATOR    "\n"
+#define USAGE_HEADER _("\nUsage:\n")
+#define USAGE_OPTIONS _("\nOptions:\n")
+#define USAGE_FUNCTIONS _("\nFunctions:\n")
+#define USAGE_COMMANDS _("\nCommands:\n")
+#define USAGE_ARGUMENTS _("\nArguments:\n")
+#define USAGE_COLUMNS _("\nAvailable output columns:\n")
+#define USAGE_SEPARATOR "\n"
 
-#define USAGE_OPTSTR_HELP     _("display this help")
-#define USAGE_OPTSTR_VERSION  _("display version")
+#define USAGE_OPTSTR_HELP _("display this help")
+#define USAGE_OPTSTR_VERSION _("display version")
 
 #define USAGE_HELP_OPTIONS(marg_dsc) \
-        "%-" #marg_dsc "s%s\n" \
-        "%-" #marg_dsc "s%s\n" \
-        , " -h, --help",    USAGE_OPTSTR_HELP \
-        , " -V, --version", USAGE_OPTSTR_VERSION
+    "%-" #marg_dsc "s%s\n"           \
+    "%-" #marg_dsc "s%s\n",          \
+        " -h, --help", USAGE_OPTSTR_HELP, " -V, --version", USAGE_OPTSTR_VERSION
 
-#define USAGE_ARG_SEPARATOR    "\n"
-#define USAGE_ARG_SIZE(_name) \
-        _(" %s arguments may be followed by the suffixes for\n" \
-          "   GiB, TiB, PiB, EiB, ZiB, and YiB (the \"iB\" is optional)\n"), _name
+#define USAGE_ARG_SEPARATOR "\n"
+#define USAGE_ARG_SIZE(_name)                                            \
+    _(" %s arguments may be followed by the suffixes for\n"              \
+      "   GiB, TiB, PiB, EiB, ZiB, and YiB (the \"iB\" is optional)\n"), \
+        _name
 
-#define USAGE_MAN_TAIL(_man)   _("\nFor more details see %s.\n"), _man
+#define USAGE_MAN_TAIL(_man) _("\nFor more details see %s.\n"), _man
 
-#define UTIL_LINUX_VERSION _("%s from %s\n"), program_invocation_short_name, PACKAGE_STRING
+#define ION_VERSION _("%s from %s\n"), program_invocation_short_name, PACKAGE_STRING
 
-#define print_version(eval) __extension__ ({ \
-        printf(UTIL_LINUX_VERSION); \
-        exit(eval); \
+#define print_version(eval) __extension__({ \
+    printf(ION_VERSION);                    \
+    exit(eval);                             \
 })
 
 /*
  * seek stuff
  */
 #ifndef SEEK_DATA
-# define SEEK_DATA	3
+#define SEEK_DATA 3
 #endif
 #ifndef SEEK_HOLE
-# define SEEK_HOLE	4
+#define SEEK_HOLE 4
 #endif
-
 
 /*
  * Macros to convert #define'itions to strings, for example
@@ -406,9 +399,9 @@ static inline int xusleep(useconds_t usec)
  * mechanism of AddressSanitizer.
  */
 #if __has_feature(address_sanitizer) && __has_attribute(no_sanitize_memory) && __has_attribute(no_sanitize_address)
-# define UL_ASAN_BLACKLIST __attribute__((noinline)) __attribute__((no_sanitize_memory)) __attribute__((no_sanitize_address))
+#define UL_ASAN_BLACKLIST __attribute__((noinline)) __attribute__((no_sanitize_memory)) __attribute__((no_sanitize_address))
 #else
-# define UL_ASAN_BLACKLIST	/* nothing */
+#define UL_ASAN_BLACKLIST /* nothing */
 #endif
 
 /*
@@ -416,14 +409,14 @@ static inline int xusleep(useconds_t usec)
  * pwd buffer and in some cases it is not large enough. See POSIX and
  * getpwnam_r man page for more details.
  */
-#define UL_GETPW_BUFSIZ	(16 * 1024)
+#define UL_GETPW_BUFSIZ (16 * 1024)
 
 /*
  * Darwin or other BSDs may only have MAP_ANON. To get it on Darwin we must
  * define _DARWIN_C_SOURCE before including sys/mman.h. We do this in config.h.
  */
 #if !defined MAP_ANONYMOUS && defined MAP_ANON
-# define MAP_ANONYMOUS  (MAP_ANON)
+#define MAP_ANONYMOUS (MAP_ANON)
 #endif
 
-#endif /* UTIL_LINUX_C_H */
+#endif /* ION_C_H */
--- main.c
+++ main.c
@@ -1,28 +1,20 @@
-/*
- * ionice: set or get process io scheduling class and priority
- *
- * Copyright (C) 2005 Jens Axboe <jens@axboe.dk>
- *
- * Released under the terms of the GNU General Public License version 2
- *
- */
-#include <stdio.h>
-#include <stdlib.h>
+#include <ctype.h>
 #include <errno.h>
 #include <getopt.h>
-#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <sys/syscall.h>
-#include <ctype.h>
+#include <unistd.h>
 
 #define HAVE_FSYNC
 #define PACKAGE_STRING "outer space"
-#define UTIL_LINUX_VERSON "1.2.3"
-#define HAVE_PROGRAM_INVOCATION_SHORT_NAME "ionice"
+#define ION_VERSON "1.0.0"
+#define HAVE_PROGRAM_INVOCATION_SHORT_NAME "ion"
 
-#include "nls.h"
-#include "strutils.h"
 #include "c.h"
 #include "closestream.h"
+#include "nls.h"
+#include "strutils.h"
 
 static int tolerant;
 
@@ -49,21 +41,21 @@ enum {
     IOPRIO_WHO_USER,
 };
 
-#define IOPRIO_CLASS_SHIFT	(13)
-#define IOPRIO_PRIO_MASK	((1UL << IOPRIO_CLASS_SHIFT) - 1)
+#define IOPRIO_CLASS_SHIFT (13)
+#define IOPRIO_PRIO_MASK ((1UL << IOPRIO_CLASS_SHIFT) - 1)
 
-#define IOPRIO_PRIO_CLASS(mask)	((mask) >> IOPRIO_CLASS_SHIFT)
-#define IOPRIO_PRIO_DATA(mask)	((mask) & IOPRIO_PRIO_MASK)
-#define IOPRIO_PRIO_VALUE(class, data)	(((class) << IOPRIO_CLASS_SHIFT) | data)
+#define IOPRIO_PRIO_CLASS(mask) ((mask) >> IOPRIO_CLASS_SHIFT)
+#define IOPRIO_PRIO_DATA(mask) ((mask)&IOPRIO_PRIO_MASK)
+#define IOPRIO_PRIO_VALUE(class, data) (((class) << IOPRIO_CLASS_SHIFT) | data)
 
-static const char *to_prio[] = {
+static const char* to_prio[] = {
     [IOPRIO_CLASS_NONE] = "none",
-    [IOPRIO_CLASS_RT]   = "realtime",
-    [IOPRIO_CLASS_BE]   = "best-effort",
+    [IOPRIO_CLASS_RT] = "realtime",
+    [IOPRIO_CLASS_BE] = "best-effort",
     [IOPRIO_CLASS_IDLE] = "idle"
 };
 
-static int parse_ioclass(const char *str)
+static int parse_ioclass(const char* str)
 {
     size_t i;
 
@@ -81,14 +73,14 @@ static void ioprio_print(int pid, int who)
         err(EXIT_FAILURE, _("ioprio_get failed"));
     else {
         int ioclass = IOPRIO_PRIO_CLASS(ioprio);
-        const char *name = _("unknown");
+        const char* name = _("unknown");
 
-        if (ioclass >= 0 && (size_t) ioclass < ARRAY_SIZE(to_prio))
+        if (ioclass >= 0 && (size_t)ioclass < ARRAY_SIZE(to_prio))
             name = to_prio[ioclass];
 
         if (ioclass != IOPRIO_CLASS_IDLE)
             printf(_("%s: prio %lu\n"), name,
-                   IOPRIO_PRIO_DATA(ioprio));
+                IOPRIO_PRIO_DATA(ioprio));
         else
             printf("%s\n", name);
     }
@@ -97,7 +89,7 @@ static void ioprio_print(int pid, int who)
 static void ioprio_setid(int which, int ioclass, int data, int who)
 {
     int rc = ioprio_set(who, which,
-                IOPRIO_PRIO_VALUE(ioclass, data));
+        IOPRIO_PRIO_VALUE(ioclass, data));
 
     if (rc == -1 && !tolerant)
         err(EXIT_FAILURE, _("ioprio_set failed"));
@@ -105,21 +97,24 @@ static void ioprio_setid(int which, int ioclass, int data, int who)
 
 static void __attribute__((__noreturn__)) usage(void)
 {
-    FILE *out = stdout;
+    FILE* out = stdout;
     fputs(USAGE_HEADER, out);
-    fprintf(out,  _(" %1$s [options] -p <pid>...\n"
-            " %1$s [options] -P <pgid>...\n"
-            " %1$s [options] -u <uid>...\n"
-            " %1$s [options] <command>\n"), program_invocation_short_name);
+    fprintf(out, _(" %1$s [options] -p <pid>...\n"
+                   " %1$s [options] -P <pgid>...\n"
+                   " %1$s [options] -u <uid>...\n"
+                   " %1$s [options] <command>\n"),
+        program_invocation_short_name);
 
     fputs(USAGE_SEPARATOR, out);
     fputs(_("Show or change the I/O-scheduling class and priority of a process.\n"), out);
 
     fputs(USAGE_OPTIONS, out);
     fputs(_(" -c, --class <class>    name or number of scheduling class,\n"
-        "                          0: none, 1: realtime, 2: best-effort, 3: idle\n"), out);
+            "                          0: none, 1: realtime, 2: best-effort, 3: idle\n"),
+        out);
     fputs(_(" -n, --classdata <num>  priority (0..7) in the specified scheduling class,\n"
-        "                          only for the realtime and best-effort classes\n"), out);
+            "                          only for the realtime and best-effort classes\n"),
+        out);
     fputs(_(" -p, --pid <pid>...     act on these already running processes\n"), out);
     fputs(_(" -P, --pgid <pgrp>...   act on already running processes in these groups\n"), out);
     fputs(_(" -t, --ignore           ignore failures\n"), out);
@@ -128,26 +123,26 @@ static void __attribute__((__noreturn__)) usage(void)
     fputs(USAGE_SEPARATOR, out);
     printf(USAGE_HELP_OPTIONS(24));
 
-    printf(USAGE_MAN_TAIL("ionice(1)"));
+    printf(USAGE_MAN_TAIL("ion(1)"));
 
     exit(EXIT_SUCCESS);
 }
 
-int main(int argc, char **argv)
+int main(int argc, char** argv)
 {
     int data = 4, set = 0, ioclass = IOPRIO_CLASS_BE, c;
     int which = 0, who = 0;
-    const char *invalid_msg = NULL;
+    const char* invalid_msg = NULL;
 
     static const struct option longopts[] = {
         { "classdata", required_argument, NULL, 'n' },
-        { "class",     required_argument, NULL, 'c' },
-        { "help",      no_argument,       NULL, 'h' },
-        { "ignore",    no_argument,       NULL, 't' },
-        { "pid",       required_argument, NULL, 'p' },
-        { "pgid",      required_argument, NULL, 'P' },
-        { "uid",       required_argument, NULL, 'u' },
-        { "version",   no_argument,       NULL, 'V' },
+        { "class", required_argument, NULL, 'c' },
+        { "help", no_argument, NULL, 'h' },
+        { "ignore", no_argument, NULL, 't' },
+        { "pid", required_argument, NULL, 'p' },
+        { "pgid", required_argument, NULL, 'P' },
+        { "uid", required_argument, NULL, 'u' },
+        { "version", no_argument, NULL, 'V' },
         { NULL, 0, NULL, 0 }
     };
 
@@ -165,7 +160,7 @@ int main(int argc, char **argv)
         case 'c':
             if (isdigit(*optarg))
                 ioclass = strtos32_or_err(optarg,
-                        _("invalid class argument"));
+                    _("invalid class argument"));
             else {
                 ioclass = parse_ioclass(optarg);
                 if (ioclass < 0)
@@ -178,7 +173,7 @@ int main(int argc, char **argv)
         case 'p':
             if (who)
                 errx(EXIT_FAILURE,
-                     _("can handle only one of pid, pgid or uid at once"));
+                    _("can handle only one of pid, pgid or uid at once"));
             invalid_msg = _("invalid PID argument");
             which = strtos32_or_err(optarg, invalid_msg);
             who = IOPRIO_WHO_PROCESS;
@@ -186,7 +181,7 @@ int main(int argc, char **argv)
         case 'P':
             if (who)
                 errx(EXIT_FAILURE,
-                     _("can handle only one of pid, pgid or uid at once"));
+                    _("can handle only one of pid, pgid or uid at once"));
             invalid_msg = _("invalid PGID argument");
             which = strtos32_or_err(optarg, invalid_msg);
             who = IOPRIO_WHO_PGRP;
@@ -194,7 +189,7 @@ int main(int argc, char **argv)
         case 'u':
             if (who)
                 errx(EXIT_FAILURE,
-                     _("can handle only one of pid, pgid or uid at once"));
+                    _("can handle only one of pid, pgid or uid at once"));
             invalid_msg = _("invalid UID argument");
             which = strtos32_or_err(optarg, invalid_msg);
             who = IOPRIO_WHO_USER;
@@ -212,53 +207,53 @@ int main(int argc, char **argv)
         }
 
     switch (ioclass) {
-        case IOPRIO_CLASS_NONE:
-            if ((set & 1) && !tolerant)
-                warnx(_("ignoring given class data for none class"));
-            data = 0;
-            break;
-        case IOPRIO_CLASS_RT:
-        case IOPRIO_CLASS_BE:
-            break;
-        case IOPRIO_CLASS_IDLE:
-            if ((set & 1) && !tolerant)
-                warnx(_("ignoring given class data for idle class"));
-            data = 7;
-            break;
-        default:
-            if (!tolerant)
-                warnx(_("unknown prio class %d"), ioclass);
-            break;
+    case IOPRIO_CLASS_NONE:
+        if ((set & 1) && !tolerant)
+            warnx(_("ignoring given class data for none class"));
+        data = 0;
+        break;
+    case IOPRIO_CLASS_RT:
+    case IOPRIO_CLASS_BE:
+        break;
+    case IOPRIO_CLASS_IDLE:
+        if ((set & 1) && !tolerant)
+            warnx(_("ignoring given class data for idle class"));
+        data = 7;
+        break;
+    default:
+        if (!tolerant)
+            warnx(_("unknown prio class %d"), ioclass);
+        break;
     }
 
     if (!set && !which && optind == argc)
         /*
-         * ionice without options, print the current ioprio
+         * ion without options, print the current ioprio
          */
         ioprio_print(0, IOPRIO_WHO_PROCESS);
     else if (!set && who) {
         /*
-         * ionice -p|-P|-u ID [ID ...]
+         * ion -p|-P|-u ID [ID ...]
          */
         ioprio_print(which, who);
 
-        for(; argv[optind]; ++optind) {
+        for (; argv[optind]; ++optind) {
             which = strtos32_or_err(argv[optind], invalid_msg);
             ioprio_print(which, who);
         }
     } else if (set && who) {
         /*
-         * ionice -c CLASS -p|-P|-u ID [ID ...]
+         * ion -c CLASS -p|-P|-u ID [ID ...]
          */
         ioprio_setid(which, ioclass, data, who);
 
-        for(; argv[optind]; ++optind) {
+        for (; argv[optind]; ++optind) {
             which = strtos32_or_err(argv[optind], invalid_msg);
             ioprio_setid(which, ioclass, data, who);
         }
     } else if (argv[optind]) {
         /*
-         * ionice [-c CLASS] COMMAND
+         * ion [-c CLASS] COMMAND
          */
         ioprio_setid(0, ioclass, data, IOPRIO_WHO_PROCESS);
         execvp(argv[optind], &argv[optind]);
