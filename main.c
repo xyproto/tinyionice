cd9f1ee202f8fa8645edc4637a98491bb07e8026
Alexander F. Rødseth <rodseth@gmail.com>
2021-02-02 19:25:15 +0100

    Refactor

--- main.c
+++ main.c
@@ -18,8 +18,63 @@
 #define HAVE_PROGRAM_INVOCATION_SHORT_NAME "ion"
 
 #include "c.h"
-#include "closestream.h"
+//#include "closestream.h"
 #include "nls.h"
+
+#define CLOSE_EXIT_CODE EXIT_FAILURE
+
+static inline int
+flush_standard_stream(FILE* stream)
+{
+    int fd;
+
+    errno = 0;
+
+    if (ferror(stream) != 0 || fflush(stream) != 0)
+        goto error;
+
+    /*
+     * Calling fflush is not sufficient on some filesystems
+     * like e.g. NFS, which may defer the actual flush until
+     * close. Calling fsync would help solve this, but would
+     * probably result in a performance hit. Thus, we work
+     * around this issue by calling close on a dup'd file
+     * descriptor from the stream.
+     */
+    if ((fd = fileno(stream)) < 0 || (fd = dup(fd)) < 0 || close(fd) != 0)
+        goto error;
+
+    return 0;
+error:
+    return (errno == EBADF) ? 0 : EOF;
+}
+
+/* Meant to be used atexit(close_stdout); */
+static inline void
+close_stdout(void)
+{
+    if (flush_standard_stream(stdout) != 0 && !(errno == EPIPE)) {
+        if (errno)
+            warn(_("write error"));
+        else
+            warnx(_("write error"));
+        _exit(CLOSE_EXIT_CODE);
+    }
+
+    if (flush_standard_stream(stderr) != 0)
+        _exit(CLOSE_EXIT_CODE);
+}
+
+static inline void
+close_stdout_atexit(void)
+{
+    /*
+     * Note that close stdout at exit disables ASAN to report memory leaks
+     */
+#if !defined(__SANITIZE_ADDRESS__)
+    atexit(close_stdout);
+#endif
+}
 
 /* initialize a custom exit code for all *_or_err functions */
 extern void strutils_set_exitcode(int exit_code);
