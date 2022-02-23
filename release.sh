80b8bbb7b4587caff3dcf39e50f6d6c37a8088b9
Alexander F. Rødseth <rodseth@gmail.com>
2022-02-23 11:38:05 +0100

    Add release and version scripts

---
+++ release.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+ver=1.0.1
+mkdir tinyionice-$ver
+cp -v main.c Makefile COPYING README.md tinyionice-$ver/
+tar zcvf tinyionice-$ver.tar.gz tinyionice-$ver/
+rm -r tinyionice-$ver/
---
+++ version.sh
@@ -0,0 +1,16 @@
+#!/bin/sh -e
+#
+# Self-modifying script that updates the version numbers
+#
+
+# The current version goes here, as the default value
+VERSION=${1:-'1.0.1'}
+
+if [ -z "$1" ]; then
+  echo "The current version is $VERSION, pass the new version as the first argument if you wish to change it"
+  exit 0
+fi
+
+echo "Setting the version to $VERSION"
+sed -i "s/tinyionice [[:digit:]]*\.[[:digit:]]*\.[[:digit:]]*/tinyionice $VERSION/g" main.c
+sed -i "s/[[:digit:]]*\.[[:digit:]]*\.[[:digit:]]*/$VERSION/g" release.sh "$0"
